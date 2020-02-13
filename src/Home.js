6bc452c318a1bceda0a16cec8d1e11c0db448356
Buford Eeds <bufordeeds8@gmail.com>
2020-02-13 11:51:39 -0600

    moved filter logic out of home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FilterNav from "./FilterNav";
+import { itemsArr } from './components/Images';
 import ItemCard from "./ItemCard";
 import axios from "axios";
 import { useState } from "react";
@@ -7,43 +8,33 @@ import { useEffect } from "react";
 
 const Home = ({ cart, setCart }) => {
   const [items, setItems] = useState([]);
-  const [navFilter, setNavFilter] = useState("all");
+  const [navFilter, setNavFilter] = useState("");
 
   useEffect(() => {
     const api = "http://localhost:3000/api/v1/items/";
 
-    axios.get(api).then(res => {
-      filterItems(res.data);
+    axios.get(api).then(({ data }) => {
+      const formattedData = data.map((itemObj, idx) => ({...itemObj, item_image: itemsArr[idx]}));
+      setItems(formattedData);
     });
-    // eslint-disable-next-line 
-  }, [navFilter]);
-
-  const filterItems = items => {
-    switch (navFilter) {
-      case("merch"):
-        let merch = items.filter(item => item.item_type === "merch");
-        setItems(merch);
-        break;
-      case("wings"):
-        let wings = items.filter(({item_type}) => (item_type === "bone-in wing" || item_type === "bone-less wing"));
-        setItems(wings)
-        break;
-      case("side"):
-        let sides = items.filter(item => item.item_type === "side");
-        setItems(sides);
-        break;
-      default:
-        setItems(items);
-        break;
-    };
-  };
+  }, []);
 
   return (
     <>
       <FilterNav setNavFilter={setNavFilter} />
-      {items.map(item => (
-        <ItemCard cart={cart} setCart={setCart} key={item.id} item={item} />
-      ))}
+      <div className={'home__container'}>
+        {items
+          .filter(({ item_type }) => item_type.includes(navFilter))
+          .map((item, index) => (
+            <ItemCard
+              index={index}
+              cart={cart}
+              setCart={setCart}
+              key={item.id}
+              item={item}
+            />
+          ))}
+      </div>
     </>
   );
 };
