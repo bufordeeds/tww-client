aedabf709451479359b42ab0853aa45f3d5a6c5f
Buford Eeds <bufordeeds8@gmail.com>
2020-02-13 11:49:46 -0600

    removed unused styles

--- src/FilterNav.js
+++ src/FilterNav.js
@@ -1,42 +1,25 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  }
-}));
-
 export default function FilterNav({ setNavFilter }) {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <AppBar position="sticky">
-        <Toolbar>
-          <Button onClick={() => setNavFilter("all")} color="inherit">
-            All
-          </Button>
-          <Button onClick={() => setNavFilter("wings")} color="inherit">
-            Wings
-          </Button>
-          <Button onClick={() => setNavFilter("side")} color="inherit">
-            Sides
-          </Button>
-          <Button onClick={() => setNavFilter("merch")} color="inherit">
-            Merch
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar className={"filter-nav"} position="sticky">
+      <Toolbar>
+        <Button onClick={() => setNavFilter("")} color="inherit">
+          All
+        </Button>
+        <Button onClick={() => setNavFilter("wing")} color="inherit">
+          Wings
+        </Button>
+        <Button onClick={() => setNavFilter("side")} color="inherit">
+          Sides
+        </Button>
+        <Button onClick={() => setNavFilter("merch")} color="inherit">
+          Merch
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
